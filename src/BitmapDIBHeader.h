b2ccc2fbe706760c34409f5c08e71dac5e6a5848
Ilan Prais <ilandovprais@gmail.com>
2020-08-16 20:41:54 +0300

    Update BitmapDIBHeader.h

--- src/BitmapDIBHeader.h
+++ src/BitmapDIBHeader.h
@@ -8,13 +8,13 @@ namespace bitmap {
 
     std::string _data; //string data
     int _hsize = 40; // size of header
-    int _bitmapWidth; // width of bitmap
-    int _bitmapHeight; //height of bitmap
+    int _bitmapWidth = 0; // width of bitmap
+    int _bitmapHeight = 0; //height of bitmap
     int _con = 1; //constant(always 1)
-    int _bpp; //bits per pixel (8 or 24)
+    int _bpp = 0; //bits per pixel (8 or 24)
     int _cm = 0; //compression method (no compression, therfore 0)
-    int bmsize; //bitmap size;
-    int numcolors; //number of colors in color
+    int bmsize = 0; //bitmap size;
+    int numcolors = 0; //number of colors in color
 
     public:
         explicit BitmapDIBHeader(std::string data);
