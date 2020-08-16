0abc8feb25b997868b34e44e28887aa7c37477e6
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2020-08-17 00:24:40 +0300

    Adding virtual to the what() methods

--- src/ErrorCode.hpp
+++ src/ErrorCode.hpp
@@ -9,7 +9,7 @@ namespace matrix {
      * @brief This class defines a null pointer exception
      */
     class NullPointerException : public std::exception {
-        const char* what() const throw() override {
+        virtual const char* what() const throw() override {
             return error_getErrorMessage(ERROR_NULL_POINTER);
         }
     };
@@ -18,7 +18,7 @@ namespace matrix {
      * @brief This class defines a null pointer exception
      */
     class MatrixInvalidOperationException : public std::exception {
-        const char* what() const throw() override {
+        virtual const char* what() const throw() override {
             return error_getErrorMessage(ERROR_MATRIX_INVALID_OPERATION);
         }
     };
