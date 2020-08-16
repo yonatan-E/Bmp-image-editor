a0e49ca147ec93247f77c420b7af86758cfef629
Ilan Prais <ilandovprais@gmail.com>
2020-08-16 20:10:13 +0300

    Adding BitmapDIBHeader.h class members

---
+++ src/BitmapDIBHeader.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <string>
+
+
+namespace bitmap {
+
+    class BitmapDIBHeader{
+
+    std::string _data; //string data
+    int _hsize = 40; // size of header
+    int _bitmapWidth; // width of bitmap
+    int _bitmapHeight; //height of bitmap
+    int _con = 1; //constant(always 1)
+    int _bpp; //bits per pixel (8 or 24)
+    int _cm = 0; //compression method (no compression, therfore 0)
+    int bmsize; //bitmap size;
+    int numcolors; //number of colors in color pallete
+
+    public:
+        explicit BitmapDIBHeader(std::string data);
+
+        BitmapDIBHeader(const BitmapDIBHeader& other);
+        BitmapDIBHeader& operator=(const BitmapDIBHeader& other);
+
+        BitmapDIBHeader(BitmapDIBHeader&& other) noexcept;
+        BitmapDIBHeader& operator=(BitmapDIBHeader&& other) noexcept;
+
+        ~BitmapDIBHeader();
+
+
+    }
