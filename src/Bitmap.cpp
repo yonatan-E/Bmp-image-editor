ba087317265fc4ad3b36375dc6a7c0c80f0a5be8
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2020-08-16 18:03:41 +0300

    Adding fields to Bitmap class

---
+++ src/Bitmap.cpp
@@ -0,0 +1,5 @@
+#include "Bitmap.h"
+
+namespace bitmap {
+    
+}
--- src/Bitmap.h
+++ src/Bitmap.h
@@ -5,7 +5,13 @@
 namespace bitmap {
 
 class Bitmap {
-
+    // the header of the bitmap file
+    BitmapHeader _header;
+    // the DIB header of the bitmap file
+    BitmapDIBHeader _dibHeader;
+    // the bitmap array
+    BitmapArray _bitmapArray;
+    // the path to the bitmap file
     std::string _path;
 
     public:
