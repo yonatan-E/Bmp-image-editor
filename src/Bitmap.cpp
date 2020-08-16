95c9b759fbfcb0e730c206dae60766ced0861a1f
Ilan Prais <ilandovprais@gmail.com>
2020-08-16 22:07:22 +0300

    Implementation of constructors, operators, and reset

--- src/Bitmap.cpp
+++ src/Bitmap.cpp
@@ -1,5 +1,73 @@
 #include "Bitmap.h"
+#include <fstream>
+#include <iterator>
 
 namespace bitmap {
+
+    Bitmap::Bitmap(std::string path){
+        this->_path = path;
+        read();
+    }
+
+    Bitmap::Bitmap(const Bitmap& other){
+        this->_path = other._path;
+        this->_header = new BitmapHeader(other._header);
+        this->_dibHeader = new BitmapDIBHeader(other._dibHeader);
+        this->_bitmapArray = new BitmapArray(other._bitmapArray);
+    }
+
+    Bitmap& Bitmap::operator=(const Bitmap& other){
+        if (this == &other) {
+		    return *this;
+	    }
+
+        reset();
+
+        *this = Bitmap(other); 
+	    return *this;
+    }
+
+    Bitmap::Bitmap(Bitmap&& other) noexcept{
+
+        _path = std::exchange(other._path, nullptr);
+        _header = std::exchange(other._header, nullptr);
+        _dibHeader = std::exchange(other._dibHeader, nullptr);
+        _bitmapArray = std::exchange(other._bitmapArray, nullptr);
+    }
+    Bitmap& Bitmap::operator=(Bitmap&& other) noexcept{
+        if (this == &other) {
+		    return *this;
+	    }
+
+	    reset();
+
+        _path = std::exchange(other._path, nullptr);
+        _header = std::exchange(other._header, nullptr);
+        _dibHeader = std::exchange(other._dibHeader, nullptr);
+        _bitmapArray = std::exchange(other._bitmapArray, nullptr);
+        return *this;
+    }
+
+    Bitmap::~Bitmap(){
+        try {
+		    reset();
+	    }
+	    catch (...) {
+
+	    }
+    }
+
+    void Read(){
+        const std::string &rfpath = this->_path; 
+        std::ifstream inp{rfpath, std::ios_base::binary };
+    }
+
+    void Bitmap::reset() noexcept{
+        delete _header;
+        delete _dibHeader;
+        delete _bitmapArray;
+
+        _path = nullptr;
+    }
     
 }
