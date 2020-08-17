6fdc112ab3dfe8fe58690561bd73aff39f61e11f
Ilan Prais <ilandovprais@gmail.com>
2020-08-17 13:30:43 +0300

    New BitAdjuster abstract class

---
+++ src/BitAdjuster.cpp
@@ -0,0 +1,18 @@
+
+namespace bitmap {
+
+    class BitAdjuster{
+
+    public: 
+
+         virtual void read() = 0;
+
+         virtual void write() = 0;
+
+         virtual void turn() = 0;
+
+         virtual void gray() = 0;
+
+    };
+
+}
