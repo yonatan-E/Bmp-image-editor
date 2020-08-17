1a8bbf37f3dba0013b7eadbe142bf1e3ded2e423
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2020-08-17 09:27:36 +0300

    Changing the exceptions in Matrix.cpp

--- src/Matrix.cpp
+++ src/Matrix.cpp
@@ -8,14 +8,14 @@ namespace matrix {
     Matrix::Matrix(uint32_t height, uint32_t width) {
         ErrorCode error = matrix_create(&this->_decorated, height, width);
         if (!error_getErrorMessage(error)) {
-            throw NullPointerException();
+            throw Exception(error);
         }
     }
 
     Matrix::Matrix(const Matrix& other) {
         ErrorCode error = matrix_copy(&this->_decorated, other._decorated);
         if (!error_getErrorMessage(error)) {
-            throw NullPointerException();
+            throw Exception(error);
         }
     }
 
