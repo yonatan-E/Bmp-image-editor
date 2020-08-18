2747ee63e897fb20c9035114db8bfbab61f69f3c
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2020-08-18 15:12:04 +0300

    Adding marks to BitAdjuster.hpp

--- src/BitAdjuster.hpp
+++ src/BitAdjuster.hpp
@@ -10,14 +10,31 @@ namespace bitmap {
 
     protected: 
          
+         /**
+          * @brief Method that reads the content of the string into the current object
+          * 
+          */
          virtual void read() = 0;
 
+         /**
+          * @brief Method that reads the content of the current object into the string
+          * 
+          */
          virtual void write() = 0;
 
     public: 
 
+         /**
+          * @brief Method that changes the content of the current object according to the turn
+          * 
+          */
          virtual void turn() = 0;
 
+         /**
+          * @brief Method that changes the content of the current object according to the
+                   color changing to gray
+          * 
+          */
          virtual void gray() = 0;
 
     };
