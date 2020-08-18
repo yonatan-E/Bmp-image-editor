a4ae3b46b13bfd1a268d79e989276182eb1dfb3a
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2020-08-18 15:12:34 +0300

    Adding marks to BitAdjuster.hpp

--- src/BitAdjuster.hpp
+++ src/BitAdjuster.hpp
@@ -2,18 +2,39 @@
 
 namespace bitmap {
 
-    class BitAdjuster{
+    /**
+     * @brief This class is a base class for all of the classes which define
+     *        the parts of the bmp file format.
+     */
+    class BitAdjuster {
 
     protected: 
-
+         
+         /**
+          * @brief Method that reads the content of the string into the current object
+          * 
+          */
          virtual void read() = 0;
 
+         /**
+          * @brief Method that reads the content of the current object into the string
+          * 
+          */
          virtual void write() = 0;
 
     public: 
 
+         /**
+          * @brief Method that changes the content of the current object according to the turn
+          * 
+          */
          virtual void turn() = 0;
 
+         /**
+          * @brief Method that changes the content of the current object according to the
+                   color changing to gray
+          * 
+          */
          virtual void gray() = 0;
 
     };
