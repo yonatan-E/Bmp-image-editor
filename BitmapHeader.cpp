219a5d7407159ce826239a2d84699555e6ec403d
Ilan Prais <ilandovprais@gmail.com>
2020-08-20 11:24:13 +0300

    Minor Fix

--- BitmapHeader.cpp
+++ BitmapHeader.cpp
@@ -3,12 +3,12 @@
 
 namespace bitmap {
 
-Bitmap::BitmapHeader(const std::string& data) : BitAdjuster(data) {
+BitmapHeader::BitmapHeader(const std::string& data) : BitAdjuster(std::move(data)) {
     // reading the data string into the current object
     this->read();
 }
 
-void Bitmap::read() {
+void BitmapHeader::read() {
     this->_fileSize = this->bytesToInteger(2, 4);
     this->_reserved1 = this->bytesToInteger(6, 2);
     this->_reserved2 = this->bytesToInteger(8, 2);
