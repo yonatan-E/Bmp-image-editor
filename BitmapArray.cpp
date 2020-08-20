6c5d02f31d44ba5ef10fa610b3fecc5863f7fd94
Ilan Prais <ilandovprais@gmail.com>
2020-08-20 12:47:59 +0300

    developing constructor and read

--- BitmapArray.cpp
+++ BitmapArray.cpp
@@ -3,13 +3,41 @@
 
 namespace bitmap {
 
-BitmapArray::BitmapArray(const std::string& data) : BitAdjuster(data) {
+BitmapArray::BitmapArray(const std::string& data, uint32_t bpp, uint32_t height, uint32_t width, ColorPallete colors) : BitAdjuster(data) {
+    
+    this->bitsPerPixel = bpp;
+    this->height = height;
+    this->width = width;
+    this->colors = colors;
+    this->pixels(height, width);
+    
     // reading the data string into the current object
     this->read();
 }
 
 void BitmapArray::read() {
-    
+
+    if(bitsPerPixel == 8){
+        for(uint32_t i = 0; i <= height ; i++){
+            for(uint32_t j = 0; j <= width ; j++){
+                pixels.set(i, j, bytesToInteger(j + i*width, 1));
+            }
+            //padding
+        }   
+    }
+
+    if(bitsPerPixel == 24){
+        uint32_t index = 0;
+        for(uint32_t i = 0; i <= height ; i++){
+            for(uint32_t j = 0; j <= width ; j+=3){
+                pixels.set(i, j, index);
+                uint8_t color[3] = {bytesToInteger(j + i*width, 1), bytesToInteger(j + i*width + 1, 1), bytesToInteger(j + i*width + 2, 1)};
+                colors.set(index, color);
+                index++;
+            }
+            //padding
+        }  
+    }
 }
 
 }
