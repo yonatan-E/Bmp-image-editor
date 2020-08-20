f7c225b8bc6f7b1ee95377995358ba7f9fddb67f
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2020-08-20 12:53:10 +0300

    Adding readFromFile and writeFileContent to .hpp

--- Bitmap.hpp
+++ Bitmap.hpp
@@ -16,14 +16,48 @@ class Bitmap: public BitAdjuster {
     ColorPallete _colorPallete;
 
     public:
+
+        /**
+         * @brief Construct a new Bitmap object
+         * 
+         * @param path the path to the bitmap file
+         */
         explicit Bitmap(std::string path);
 
+        /**
+         * @brief The copy constructor
+         * 
+         * @param other the copied object
+         */
         Bitmap(const Bitmap& other);
-        Bitmap& operator=(const Bitmap& other);
 
+        /**
+         * @brief The copy assignment operator
+         * 
+         * @param other the copied object
+         * @return Bitmap& the current object
+         */
+        Bitmap& operator=(const Bitmap& other);
+ 
+        /**
+         * @brief The move constructor
+         * 
+         * @param other the moved object
+         */
         Bitmap(Bitmap&& other) noexcept;
+
+        /**
+         * @brief The move assignment operator
+         * 
+         * @param other the moved object
+         * @return Bitmap& the current object
+         */
         Bitmap& operator=(Bitmap&& other) noexcept;
 
+        /**
+         * @brief Destroy the Bitmap object
+         * 
+         */
         ~Bitmap();
 
         void turn();
@@ -37,8 +71,28 @@ class Bitmap: public BitAdjuster {
         void write();
 
     private:
-
+        
+        /**
+         * @brief Reset the fields of the bitmap object
+         * 
+         */
         void reset() noexcept;
+        
+        /**
+         * @brief Read the content of a file
+         * 
+         * @param path the path to the file
+         * @return std::string the content of the file
+         */
+        std::string readFromFile(const std::string& path);
+
+        /**
+         * @brief write the content to a file
+         * 
+         * @param filePath the path to the file
+         * @param content the content to write in the file
+         */
+        void writeFileContent(const std::string& filePath, const std::string& content)
 
 };
 
