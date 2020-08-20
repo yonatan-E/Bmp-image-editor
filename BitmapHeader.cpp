2c910bc4ddfe717402b41ffea65f6a72164ad8f3
Ilan Prais <ilandovprais@gmail.com>
2020-08-20 10:46:31 +0300

    changes function namespaces to Bitmap

--- BitmapHeader.cpp
+++ BitmapHeader.cpp
@@ -3,12 +3,12 @@
 
 namespace bitmap {
 
-BitmapHeader::BitmapHeader(const std::string& data) : BitAdjuster(data) {
+Bitmap::BitmapHeader(const std::string& data) : BitAdjuster(data) {
     // reading the data string into the current object
     this->read();
 }
 
-void BitmapHeader::read() {
+void Bitmap::read() {
     this->_fileSize = this->bytesToInteger(2, 4);
     this->_reserved1 = this->bytesToInteger(6, 2);
     this->_reserved2 = this->bytesToInteger(8, 2);
