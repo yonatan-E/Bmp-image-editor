82b722cc5a181dfc188acac422155ac20ae01440
Ilan Prais <ilandovprais@gmail.com>
2020-08-21 12:03:53 +0300

    tester for writing into the file

--- tester.cpp
+++ tester.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <fstream>
 #include <iterator>
+#include <cstdint>
+#include <cstring>
 
 std::string readFileContent(const std::string& filePath) {
   // Opens input-only file (ifstream) in binary mode.
@@ -28,11 +30,50 @@ std::string readFileContent(const std::string& filePath) {
   return content;
 }
 
+void writeFileContent(const std::string& filePath, const std::string& content) {
+  // Opens output-only file (ofstream) in binary mode, and truncates all
+  //    existing content from the file.
+  std::ofstream out(filePath, std::ios::binary | std::ios::trunc);
+
+  // The file is in a bad state.
+  if (!out) {
+    // Error here...
+  }
+
+  out.write(content.data(), static_cast<std::streamsize>(content.length()));
+  if (!out) {
+    // Unlikly to happen error here...
+  }
+}
+
+
+void intToByte(int n, unsigned char c[4]) {
+    c[0] =(unsigned char)(n);
+    c[1] = (unsigned char)(n >>  8);
+    c[2] = (unsigned char)(n >> 16);
+    c[3] = (unsigned char)(n >> 24);
+}
+
+
 int main() {
-  std::string result = readFileContent("lena-color.bmp");
-  //  std::cout << result;
-  //bitmap::BitmapHeader header(result.substr(0, 14));
-  int pr = *((uint32_t*)(result.substr(14, 4).data()));
+
+  std::string f1 = readFileContent("lena-color.bmp");
+    char header[4];
+    int x = 50;
+
+    std::memcpy(header, &x, 4);
+    std::string str = static_cast<char*>(header);
+    str = str + (char)0 +  (char)0;
+
+    std::string resultW = f1.substr(0, 14) + str + f1.substr(18);
+ 
+    writeFileContent("lena-color.bmp", resultW); //Writing 50 insted of 40;
+
+    std::string result = readFileContent("lena-color.bmp");
+
+     int pr = *((uint32_t*)(result.substr(14,4).data()));
+  
   std::cout << pr << "\n";
+
   return 0;
 }
