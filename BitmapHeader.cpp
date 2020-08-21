4252f74a29b47edf33d33bc34d1fc320dd6717a6
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2020-08-21 17:14:29 +0300

    Change in ctor

--- BitmapHeader.cpp
+++ BitmapHeader.cpp
@@ -4,7 +4,7 @@
 
 namespace bitmap {
 
-BitmapHeader::BitmapHeader(const std::string& data) : BitAdjuster(data) {
+BitmapHeader::BitmapHeader(const std::string& data) : BitAdjuster(std::move(data)) {
     // reading the data string into the current object
     this->read();
 }
