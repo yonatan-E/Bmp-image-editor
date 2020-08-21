395da4076ab2b07de2dbf6fd88ce3cdc23b65a03
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2020-08-21 14:27:02 +0300

    Adding turn()

--- BitmapDIBHeader.cpp
+++ BitmapDIBHeader.cpp
@@ -1,5 +1,6 @@
 #include "BitmapDIBHeader.hpp"
 #include <string>
+#include <cstdint>
 
 namespace bitmap {
 
@@ -21,6 +22,12 @@ void BitmapDIBHeader::write(){
     + getData().substr(12, 2) + integerToBytes(this->_bpp , 2) + getData().substr(16, 4) + integerToBytes(this->_bmsize , 4)
     + getData().substr(24, 8) + integerToBytes(this->_numcolors , 4) + getData().substr(36);
     setData(newData);
+}
+
+void BitmapDIBHeader::turn() {
+    uint32_t temp = this->_bitmapHeight;
+    this->_bitmapHeight = this->_bitmapWidth;
+    this->_bitmapWidth = temp;
 }
 
 }
