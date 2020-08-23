39f21e48cf2343b62c36bf915e5943eae7dffbbf
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2020-08-23 13:18:44 +0300

    Updating main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,20 @@
 #include "BitmapHeader.hpp"
+#include <iostream>
+#include <string>
+#include <fstream>
 
-int main(){
+std::string readFileContent(const std::string& filePath) {
+    std::ifstream in(filePath, std::ios::binary);
 
-    std::string str = readFileContent("lena.bmp");
-    bitmap::BitmapHeader bh = new bitmap::BitmapHeader(str.substr(0, 14));
-    
+    if (!in) {}
 
+    auto content = std::string{std::istreambuf_iterator<char>{in}, std::istreambuf_iterator<char>{}};
+
+    if (!in.eof()) {}
 }
 
-std::string readFileContent(const std::string& filePath) { 
-        std::ifstream in(filePath, std::ios::binary);
-
-        if (!in) {
-        // Error here...
-        }
-
-        auto content = std::string{std::istreambuf_iterator<char>{in},
-                             std::istreambuf_iterator<char>{}};
-
-        if (!in.eof()) {
-          // Unlikly to happen error here...
-        }
-
-        return content;
-    }
+int main() {
+    std::string str = readFileContent("lena.bmp");
+    bitmap::BitmapHeader bh(str.substr(0, 14));
+    std::cout << bh.getOffset();
+}
