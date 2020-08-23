3da4c45782d7dbff56dfa7f3c84dfd97daca2548
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2020-08-23 20:33:49 +0300

    Changing design

--- BitmapDIBHeader.cpp
+++ BitmapDIBHeader.cpp
@@ -6,28 +6,28 @@ namespace bitmap {
 
 BitmapDIBHeader::BitmapDIBHeader(std::string data) : BitAdjuster(std::move(data)) {
     // reading the data string into the current object
-    this->read();
+    read();
 }
 
 void BitmapDIBHeader::read() {
-    this->_bitmapWidth = bytesToInteger<uint32_t>(4);
-    this->_bitmapHeight = bytesToInteger<uint32_t>(8);
-    this->_bpp = bytesToInteger<uint16_t>(14);
-    this->_bmsize = bytesToInteger<uint32_t>(20);
-    this->_numcolors = bytesToInteger<uint32_t>(32);
+    _bitmapWidth = bytesToInteger<uint32_t>(4);
+    _bitmapHeight = bytesToInteger<uint32_t>(8);
+    _bpp = bytesToInteger<uint16_t>(14);
+    _bmsize = bytesToInteger<uint32_t>(20);
+    _numcolors = bytesToInteger<uint32_t>(32);
 }
 
 void BitmapDIBHeader::write() {
-    std::string newData = getData().substr(0,4) + integerToBytes<uint32_t>(this->_bitmapWidth) + integerToBytes<uint32_t>(this->_bitmapHeight)
-    + getData().substr(12, 2) + integerToBytes<uint16_t>(this->_bpp) + getData().substr(16, 4) + integerToBytes<uint32_t>(this->_bmsize)
-    + getData().substr(24, 8) + integerToBytes<uint32_t>(this->_numcolors) + getData().substr(36);
+    std::string newData = getData().substr(0,4) + integerToBytes<uint32_t>(_bitmapWidth) + integerToBytes<uint32_t>(_bitmapHeight)
+    + getData().substr(12, 2) + integerToBytes<uint16_t>(_bpp) + getData().substr(16, 4) + integerToBytes<uint32_t>(_bmsize)
+    + getData().substr(24, 8) + integerToBytes<uint32_t>(_numcolors) + getData().substr(36);
     setData(newData);
 }
 
 void BitmapDIBHeader::turn() {
-    uint32_t temp = this->_bitmapHeight;
-    this->_bitmapHeight = this->_bitmapWidth;
-    this->_bitmapWidth = temp;
+    uint32_t temp = _bitmapHeight;
+    _bitmapHeight = _bitmapWidth;
+    _bitmapWidth = temp;
 }
 
 void BitmapDIBHeader::gray() {}
