d629b39ad5973a79f5da555297fcca85baf15d0f
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2020-08-23 13:42:10 +0300

    Change in tester.cpp

--- tester.cpp
+++ tester.cpp
@@ -19,6 +19,6 @@ int main() {
     std::string result = readFileContent("lena.bmp");
     bitmap::ColorPallete colors(result.substr(54 , 1078 - 54));
     for (auto i = 0; i < colors._colors.size(); i++) {
-        std::cout << colors._colors[i][0] << colors._colors[i][1] << colors._colors[i][2] << std::endl;
+        std::cout << colors._colors[i][0] << " " << colors._colors[i][1] << " " << colors._colors[i][2] << std::endl;
     }
 }
