ed0a9f6f50bb0a38acf25a10151d8b44324f4fed
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2020-08-23 16:08:28 +0300

    Fixing write()

--- tester.cpp
+++ tester.cpp
@@ -17,8 +17,20 @@ std::string readFileContent(const std::string& filePath) {
 
 int main() {
     std::string result = readFileContent("lena.bmp");
-    bitmap::ColorPallete colors(result.substr(54 , 1078 - 54));
+    bitmap::ColorPallete colors("");
+    for (auto i = 30; i < 80; i++) {
+        colors.addColor(i, i-20, i+20);
+    }
     for (auto i = 0; i < colors._colors.size(); i++) {
         std::cout << colors._colors[i][0] << " " << colors._colors[i][1] << " " << colors._colors[i][2] << std::endl;
-    }
+    } std::cout << '\n';
+
+    colors.gray();
+    colors.write();
+
+    bitmap::ColorPallete colors2(colors.getData());
+    for (auto i = 0; i < colors2._colors.size(); i++) {
+        std::cout << colors2._colors[i][0] << " " << colors2._colors[i][1] << " " << colors2._colors[i][2] << std::endl;
+    } std::cout << '\n';
+
 }
