4d7516f4279905e968992ae10b08a0309ba18e65
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2020-08-24 20:57:03 +0300

    Rebuilding

--- src/BitmapHeader.cpp
+++ src/BitmapHeader.cpp
@@ -10,7 +10,7 @@ namespace bitmap {
 BitmapHeader::BitmapHeader(std::string data) 
     : BitAdjuster(std::move(data)), _fileSize(bytesToInteger<uint32_t>(2)), _reserved1(bytesToInteger<uint16_t>(6)),
     _reserved2(bytesToInteger<uint16_t>(8)), _offset(bytesToInteger<uint32_t>(10)) {
-        // a file in BMP format must ftart with 0x4D42
+        // a file in BMP format must start with 0x4D42
         if (bytesToInteger<uint16_t>(0) != 0x4D42) {
             throw std::runtime_error("The file is not in the correct BMP format");
         }
