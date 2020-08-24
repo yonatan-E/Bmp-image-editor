70e3c16933d90c8d016600a9b47c40063015ac8b
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2020-08-24 19:29:11 +0300

    Adding bmp_tester files and changing cmake

---
+++ bmp_tester.hpp
@@ -0,0 +1,27 @@
+#include <string>
+
+namespace testing {
+namespace bmp {
+/**
+ * @brief Receives a path to a BMP image on the disk and rotates it 90 degrees clockwise,
+ *  writing the resulting image to the disk.
+ *
+ * @param imagePath Path of a BMP image that should be rotated. The path might be invalid
+ *  or contain a non-BMP file.
+ * @param outputPath Path of the resulting BMP image. can be assumed to 
+ *  reside in a valid directory, however, cannot be assumed to exist. If the
+ *  path already exists, the file previously existing in the path should be
+ *  overriden.
+ */
+void rotate_image(const std::string& imagePath, const std::string& outputPath);
+
+/**
+ * @brief Receives a path to a BMP image on the disk and converts it to grayscale,
+ *  writing the resulting image to the disk.
+ * 
+ * @param imagePath Path of a BMP image that should be rotated.
+ * @param outputPath Path of the resulting BMP image.
+ */
+void convert_to_grayscale(const std::string& imagePath, const std::string& outputPath);
+}
+}
