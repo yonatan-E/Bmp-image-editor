a14054ad2473246d5c3343201a94ac0d5c94e719
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2020-08-24 20:55:10 +0300

    Adding exceptions

--- BitmapHeader.cpp
+++ BitmapHeader.cpp
@@ -3,12 +3,18 @@
 #include <cstdint>
 #include <cstring>
 #include <iostream>
+#include <stdexcept>
 
 namespace bitmap {
 
 BitmapHeader::BitmapHeader(std::string data) 
     : BitAdjuster(std::move(data)), _fileSize(bytesToInteger<uint32_t>(2)), _reserved1(bytesToInteger<uint16_t>(6)),
-    _reserved2(bytesToInteger<uint16_t>(8)), _offset(bytesToInteger<uint32_t>(10)) {}
+    _reserved2(bytesToInteger<uint16_t>(8)), _offset(bytesToInteger<uint32_t>(10)) {
+        // a file in BMP format must ftart with 0x4D42
+        if (bytesToInteger<uint16_t>(0) != 0x4D42) {
+            throw std::runtime_error("The file is not in the correct BMP format");
+        }
+    }
 
 void BitmapHeader::write() {}
 
